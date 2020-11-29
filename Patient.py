eb8fbf56f678afeb1f94337b4abf26da6a30683b
TanyaZel <73606763+TanyaZel@users.noreply.github.com>
2020-11-29 14:02:46 +0200

    Class patient
    if the test is positive , create a patient ,the patients info automatically updated from the Test Class (base and driven)

---
+++ Patient.py
@@ -0,0 +1,22 @@
+import Status
+import Test
+
+
+if Status == 1:
+
+    class Patient(Test):  # the class takes all the info from Test class , and adds status to the patient.
+        def __init__(self, status):
+            self.status = Status.status_change(self)
+
+
+
+
+
+
+
+
+
+
+
+
+
