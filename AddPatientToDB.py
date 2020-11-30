c1c1115e00e5f890ff03b589f3aa2b8222e27d6c
Max Shapira <maximsh4@ac.sce.ac.il>
2020-11-30 17:22:49 +0200

    added notes to AddPatientToDB.py and CovidConfirmMessage.py

--- AddPatientToDB.py
+++ AddPatientToDB.py
@@ -14,33 +14,32 @@ class New_Patient:
     is_tested: str
     status: str
 
-def CreateNewSheet(city):
-    def SwitchTitle(column):
-        switcher={
-            1:'Firstname',
-            2:'Lastname',
+
+def CreateNewSheet(city):  # function for creating a new 'city' sheet in xlsx database
+    def SwitchTitle(column):  # function for adding to the xlsx table columns, titles
+        switcher = {
+            1: 'Firstname',
+            2: 'Lastname',
             3: 'ID',
             4: 'Birth date',
-            5:'Test date',
-            6:'Patient Status'}
+            5: 'Test date',
+            6: 'Patient Status'}
         return switcher
 
-    Database.Covid19DB.create_sheet(city)
-    city_sheet = Database.Covid19DB[city]
-    for colmn in range(1,7):
-        city_sheet.cell(row=1, column=colmn).value =  SwitchTitle(colmn)[colmn]
+    Database.Covid19DB.create_sheet(city)  # creating a new sheet with 'city' name
+    city_sheet = Database.Covid19DB[city]  # implementing the 'city' sheet into a variable "city_sheet"
+    for colmn in range(1, 7):  # adding to the xlsx table columns, titles
+        city_sheet.cell(row=1, column=colmn).value = SwitchTitle(colmn)[colmn]
 
 
-def addPatient(New_Patient, city):
-
-    if city not in Database.Covid19DB.sheetnames:
+def addPatient(New_Patient, city):  # function for adding a patient to the 'city' sheet
+    if city not in Database.Covid19DB.sheetnames:  # if there is no 'city' sheet , calls a fucntion to create one
         CreateNewSheet(city)
 
-
-    New_Patient.is_tested=None
+    New_Patient.is_tested = None
     city_sheet = Database.Covid19DB[city]
 
-    def switch_input(k):
+    def switch_input(k):  # function to choose patient's parameters
         switcher = {
             1: New_Patient.firstname,
             2: New_Patient.lastname,
@@ -51,17 +50,15 @@ def addPatient(New_Patient, city):
         }
         return switcher
 
-    for row in range(2, 28):
-        for j in range(2, 3):
+    for row in range(2, 28):  # starts from row#2 because #1 is titles and end in #27(temporarly)
+        for j in range(1, 2):  # start in column#1 to check if its empty
             if city_sheet.cell(row=row, column=j).value is None:
-                for col in range(1, 7):
+                for col in range(1, 7):  # adding patient parameters into the empty row by columns
                     city_sheet.cell(row=row, column=col).value = switch_input(col)[col]
-                Database.Covid19DB.save('Database.xlsx')
+                Database.Covid19DB.save('Database.xlsx')  # after adding parameters,the system "autosaves" the xlsx file
                 break
             else:
                 break
-        if New_Patient.firstname == city_sheet.cell(row=row, column=1).value:
+        if New_Patient.firstname == city_sheet.cell(row=row,
+                                                    column=1).value:  # after adding a patient, stops the outer loop
             break
-
-
-
--- CovidConfirmMessage.py
+++ CovidConfirmMessage.py
@@ -17,34 +17,34 @@ class New_Patient:
 
 
 def confirm_message(New_Patient):
-    message = open('Message.txt', 'rt').read().splitlines()
+    message = open('Message.txt', 'rt').read().splitlines()   #splits lines in the Message text file to a list of lines
     print(open('Message.txt', 'rt').read())
-    patient_tested = str(input(message[4]))
+    patient_tested = str(input(message[4]))   #checks if the patient is even tested for COVID19
     if patient_tested in ['n', 'N']:
         New_Patient.is_tested = 'no'
         exit("patient not tested!")
     elif patient_tested in ['y', 'Y']:
         New_Patient.is_tested = 'yes'
-    else:
+    else:                                  #if entered wrong answer, gives a chance to try again
         if str(input("invalid input received, enter 1 to try again else 0 to exit")) == '1':
             confirm_message(New_Patient)
         else:
             exit(0)
-    patient_result = str(input(message[5]))
+    patient_result = str(input(message[5]))   #checks the patient's result for the COVID19 test
     if patient_result in ['p', 'P']:
         New_Patient.test_result = 'Positive'
     elif patient_result in ['n', 'N']:
         New_Patient.status = 'Negative'
         exit("patient test result is negative")
-    else:
+    else:                                   #if entered wrong answer, gives a chance to try again
         if str(input("invalid input received, enter 1 to try again else 0 to exit")) == '1':
             confirm_message(New_Patient)
         else:
             exit(0)
 
-    if patient_result in ['P', 'p']:
+    if patient_result in ['P', 'p']:                  #checks in which city the patient lives
         New_Patient.city = str(input("Which city the patient is from?"))
-        AddPatientToDB.addPatient(New_Patient, New_Patient.city)
+        AddPatientToDB.addPatient(New_Patient, New_Patient.city)        #adding the patient to the given city sheet
 
 
 confirm_message(New_Patient("Mike", "lasto", 546827551, datetime.strptime("25/11/1995", "%d/%m/%Y"),
