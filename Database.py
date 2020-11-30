d770bb2f3d2758fcf85cb0a4640c48edfefa0ce3
MosheShmulevich <moshesh11@ac.sce.ac.il>
2020-11-30 18:49:55 +0200

    message

--- Database.py
+++ Database.py
@@ -3,3 +3,6 @@ from openpyxl import *
 Covid19DB = load_workbook(filename='Database.xlsx')
 CityDB = ((load_workbook(filename="Israel city list.xlsx"))['Israel Cities'])['A']
 IL_CityList = [CityDB[x].value for x in range(len(CityDB))]
+
+userDb = load_workbook(filename='UserDatabase.xlsx')
+userSheet = userDb['users']
